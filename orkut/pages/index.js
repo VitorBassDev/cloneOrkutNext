2a78d6f3752209e8d1b35937c102919784f5c7c7
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-23 16:51:40 -0300

    Feat: Inclusao do Header como menu Utilizaçao do arquivo AluraKutCommons

--- orkut/pages/index.js
+++ orkut/pages/index.js
@@ -2,37 +2,42 @@
 import React from "react";
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-
+import {AlurakutMenu} from '../src/lib/AlurakutCommons'
 
 export default function Home() {
 
+  const githubUser = 'Vitorbassdev'
+
   return (
-    <MainGrid>
-      <div style={{gridArea: 'profileArea'}}>
-        <Box>
-        <img src={`https://github.com/Vitorbassdev.png`} style={{ borderRadius: '8px' }} />
-        </Box>
-      </div>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div style={{gridArea: 'profileArea'}}>
+          <Box>
+          <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+          </Box>
+        </div>
 
-      <div style={{gridArea: 'welcomeArea'}}>
-        <Box>
-          Bem Vindo
-        </Box>
-      </div>
+        <div style={{gridArea: 'welcomeArea'}}>
+          <Box>
+            Bem Vindo
+          </Box>
+        </div>
 
-      <div style={{gridArea: 'profileRelationsArea'}}>
-        <Box>
-          Pessoas da Comunidade
-        </Box>
-      </div>
+        <div style={{gridArea: 'profileRelationsArea'}}>
+          <Box>
+            Pessoas da Comunidade
+          </Box>
+        </div>
 
-      <div style={{gridArea: 'profileRelationsArea'}}>
-        <Box>
-          Comunidades
-        </Box>
-      </div>
+        <div style={{gridArea: 'profileRelationsArea'}}>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
 
-    </MainGrid>
+      </MainGrid>
+    </>
   )
 }
 
